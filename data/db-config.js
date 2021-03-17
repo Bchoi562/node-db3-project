566c27adaf343e1c0703311c919a336964533909
Brian Choi <b.choi562@gmail.com>
2021-03-17 16:17:54 -0700

    MVP

--- data/db-config.js
+++ data/db-config.js
@@ -1,1 +1,11 @@
 // db-config
+const knex = require('knex');
+
+const env = process.env.NODE_ENV || 'development';
+
+const configs = require("../knexfile");
+
+const db = knex(config[env]);
+
+module.exports = db;
+
